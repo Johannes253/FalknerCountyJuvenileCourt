b4603f80ed9e78c6fb99bfeb4379037ad54485ed
Johannes253 <johannesm763@gmail.com>
2023-11-17 13:55:59 -0600

    added authorize to tables

--- FalknerCountyJuvenileCourt/Pages/Tables/Index.cshtml.cs
+++ FalknerCountyJuvenileCourt/Pages/Tables/Index.cshtml.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
 
 namespace FalknerCountyJuvenileCourt.Pages.Tables;
 
+[Authorize]
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
