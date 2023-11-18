5b2d38c2bd9d73922c1498190e2a60a6c8c1a89b
Johannes253 <johannesm763@gmail.com>
2023-11-17 23:03:32 -0600

    first simple excel download, Juvenile works

--- FalknerCountyJuvenileCourt/Pages/ExcelDownload.cshtml.cs
+++ FalknerCountyJuvenileCourt/Pages/ExcelDownload.cshtml.cs
@@ -1,19 +1,27 @@
-using ClosedXML.Excel;
-using ExcelStar.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using FalknerCountyJuvenileCourt.Data;
+using FalknerCountyJuvenileCourt.Models;
+using ClosedXML.Excel;
 
-namespace ExcelStar.Pages;
+namespace FalknerCountyJuvenileCourt.Pages;
 
 public class ExcelDownloadModel : PageModel {
     private readonly ILogger<IndexModel> _logger;
-    private readonly List<Juvenile> Juveniles;
-
-    public ExcelModel(ILogger<IndexModel> logger) {
+    private readonly CourtContext _context;
+    public ExcelDownloadModel(ILogger<IndexModel> logger, CourtContext context) {
         _logger = logger;
+        _context = context;
     }
 
     public FileResult OnGet() {
+
+        var juveniles = _context.Juveniles.ToList();
 
         using (var workbook = new XLWorkbook()) {
             IXLWorksheet worksheet =
@@ -33,8 +41,37 @@ public class ExcelDownloadModel : PageModel {
 
             int index = 1;
 
-            
+            foreach (var juvenile in juveniles) {
+                index++;
 
+                worksheet.Cell(index, 1).Value = juvenile.ID;
+                worksheet.Cell(index, 2).Value = juvenile.FaulknerCountyIdentification;
+                worksheet.Cell(index, 3).Value = juvenile.Age;
+                worksheet.Cell(index, 4).Value = juvenile.RaceID;
+                worksheet.Cell(index, 5).Value = juvenile.Race?.ToString() ?? string.Empty;
+                worksheet.Cell(index, 6).Value = juvenile.GenderID;
+                worksheet.Cell(index, 7).Value = juvenile.Gender?.ToString() ?? string.Empty;
+                worksheet.Cell(index, 8).Value = juvenile.RiskID;
+                worksheet.Cell(index, 9).Value = juvenile.Risk?.ToString() ?? string.Empty;
+                if(juvenile.Repeat)
+                    worksheet.Cell(index, 10).Value = "Yes";
+                else
+                    worksheet.Cell(index, 10).Value = "No";
+
+
+            }
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+                    var strDate = DateTime.Now.ToString("yyyy");
+                    string filename = string.Format($"FaulknerCountyAnnualReport_{strDate}.xlsx");
+
+                    return File(content, contentType, filename);
+                }
         }
     }
 }
