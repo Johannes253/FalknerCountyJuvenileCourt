2e91630ff040513607a1954580ae985f8c6de0fe
Johannes253 <johannesm763@gmail.com>
2023-11-17 23:24:22 -0600

    doesnt show ids anymore but names instead

--- FalknerCountyJuvenileCourt/Pages/ExcelDownload.cshtml.cs
+++ FalknerCountyJuvenileCourt/Pages/ExcelDownload.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using FalknerCountyJuvenileCourt.Data;
 using FalknerCountyJuvenileCourt.Models;
 using ClosedXML.Excel;
@@ -21,7 +22,11 @@ public class ExcelDownloadModel : PageModel {
 
     public FileResult OnGet() {
 
-        var juveniles = _context.Juveniles.ToList();
+        var juveniles = _context.Juveniles
+            .Include(j => j.Race)
+            .Include(j => j.Gender)
+            .Include(j => j.Risk)
+            .ToList();
 
         using (var workbook = new XLWorkbook()) {
             IXLWorksheet worksheet =
@@ -30,14 +35,11 @@ public class ExcelDownloadModel : PageModel {
             worksheet.Cell(1, 2).Value = "Faulkner County Juvenile ID";
             worksheet.Cell(1, 3).Value = "Age";
             worksheet.Cell(1, 4).Value = "Race";
-            worksheet.Cell(1, 5).Value = "Race ID";
-            worksheet.Cell(1, 6).Value = "Gender";
-            worksheet.Cell(1, 7).Value = "Gender ID";
-            worksheet.Cell(1, 8).Value = "Risk";
-            worksheet.Cell(1, 9).Value = "Risk ID";
-            worksheet.Cell(1, 10).Value = "Repeat Offender";
+            worksheet.Cell(1, 5).Value = "Gender";
+            worksheet.Cell(1, 6).Value = "Risk";
+            worksheet.Cell(1, 7).Value = "Repeat Offender";
 
-            IXLRange range = worksheet.Range(worksheet.Cell(1, 1).Address, worksheet.Cell(1, 10).Address);
+            IXLRange range = worksheet.Range(worksheet.Cell(1, 1).Address, worksheet.Cell(1, 7).Address);
 
             int index = 1;
 
@@ -47,16 +49,13 @@ public class ExcelDownloadModel : PageModel {
                 worksheet.Cell(index, 1).Value = juvenile.ID;
                 worksheet.Cell(index, 2).Value = juvenile.FaulknerCountyIdentification;
                 worksheet.Cell(index, 3).Value = juvenile.Age;
-                worksheet.Cell(index, 4).Value = juvenile.RaceID;
-                worksheet.Cell(index, 5).Value = juvenile.Race?.ToString() ?? string.Empty;
-                worksheet.Cell(index, 6).Value = juvenile.GenderID;
-                worksheet.Cell(index, 7).Value = juvenile.Gender?.ToString() ?? string.Empty;
-                worksheet.Cell(index, 8).Value = juvenile.RiskID;
-                worksheet.Cell(index, 9).Value = juvenile.Risk?.ToString() ?? string.Empty;
+                worksheet.Cell(index, 4).Value = juvenile.Race?.ToString() ?? string.Empty;
+                worksheet.Cell(index, 5).Value = juvenile.Gender?.ToString() ?? string.Empty;
+                worksheet.Cell(index, 6).Value = juvenile.Risk?.ToString() ?? "Unknown";
                 if(juvenile.Repeat)
-                    worksheet.Cell(index, 10).Value = "Yes";
+                    worksheet.Cell(index, 7).Value = "Yes";
                 else
-                    worksheet.Cell(index, 10).Value = "No";
+                    worksheet.Cell(index, 7).Value = "No";
 
 
             }
