e14577e5241c2caad1827fa87de871f33606a3bf
Johannes253 <johannesm763@gmail.com>
2023-12-02 11:24:12 -0600

    second pie chart

--- FalknerCountyJuvenileCourt/Pages/Report.cshtml.cs
+++ FalknerCountyJuvenileCourt/Pages/Report.cshtml.cs
@@ -45,4 +45,32 @@ public class ReportModel : PageModel
             };
         }
     }
+
+    public async Task<IActionResult> OnGetGenderDistributionDataAsync()
+{
+    Console.WriteLine("Function called");
+    try
+    {
+        var juvenilesWithGender = _context.Juveniles
+            .Include(j => j.Gender)
+            .ToList();
+
+        var genderCounts = juvenilesWithGender
+            .Where(j => j.Gender != null)
+            .GroupBy(j => j.Gender.Name)
+            .Select(group => new { Gender = group.Key, Count = group.Count() })
+            .ToList();
+
+        return new JsonResult(genderCounts);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
+        return new JsonResult("An error occurred while processing the data.")
+        {
+            StatusCode = 500
+        };
+    }
+}
+
 }
