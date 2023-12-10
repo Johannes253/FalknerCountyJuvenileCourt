624b9001c8ba9c8a4a252985bd967f4018014395
Jkwashu <Jkwashu@gmail.com>
2023-12-09 18:18:16 -0600

    Styling Changes, Rest of Graphs Finished

--- FalknerCountyJuvenileCourt/Pages/Report.cshtml.cs
+++ FalknerCountyJuvenileCourt/Pages/Report.cshtml.cs
@@ -14,7 +14,37 @@ public class ReportModel : PageModel
         _context = context;
     }
 
+    public async Task<IActionResult> OnGetArrestedRaceDistributionDataAsync()
+    {
 
+        try
+        {
+            var crimes = _context.Crimes
+                .Include(c => c.Juvenile)
+                .ThenInclude(j => j.Race)
+                .ToList();
+
+            var raceCounts = crimes
+                .Select(c => c.Juvenile?.Race?.Name)
+                .Where(race => !string.IsNullOrEmpty(race))
+                .GroupBy(race => race)
+                .Select(group => new { Race = group.Key, Count = group.Count() })
+                .ToList();
+                foreach (var race in raceCounts)
+                    Console.WriteLine($"Race: {race.Race}, Count: {race.Count}");
+
+
+            return new JsonResult(raceCounts);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            return new JsonResult("An error occurred while processing the data.")
+            {
+                StatusCode = 500
+            };
+        }
+    }
     public async Task<IActionResult> OnGetGenderDistributionDataAsync()
     {
 
